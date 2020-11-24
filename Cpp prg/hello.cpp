cb8cf04de763402a577dfa87b678960be03d685a
Arka Dutta <arkadutta.cg@gmail.com>
2020-11-24 23:28:19 +0530

    Initial Commit

---
+++ Cpp prg/hello.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello World";
+    cout<<" Enter 2 Nos \n";
+    int a,b;
+    cin>>a>>b;
+    cout<<"The Sum is = "<<(a+b);
+}
